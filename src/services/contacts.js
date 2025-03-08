6df99d6e41325744c9756a55398564d02b1e39b6
Nickolaythebest <156540277+Nickolaythebest@users.noreply.github.com>
2025-03-08 23:45:34 +0100

    repaired delete

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -38,8 +38,6 @@ export const updateContact = async (contactId, payload, options = {}) => {
 };
 
 export const deleteContact = async (contactId) => {
-  const contact = ContactsCollection.findByIdAndDelete({
-    _id: contactId,
-  });
+  const contact = await ContactsCollection.findByIdAndDelete(contactId);
   return contact;
 };
