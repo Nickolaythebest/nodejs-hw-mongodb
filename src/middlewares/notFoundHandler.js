67fb7832439b3717c0391c59cc7ef0905bc7208e
Nickolaythebest <156540277+Nickolaythebest@users.noreply.github.com>
2025-03-09 15:27:28 +0100

    Mentors error bearbeitet2

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,5 +1,5 @@
 
-export const notFoundHandler = (err, req, res, next) => {
+export const notFoundHandler = ( req, res, next) => {
     res.status(404).json({
         message: 'Route not found',
       });
