dc002e7c5d06821fe66e8f92656185120f0a087e
Nickolaythebest <156540277+Nickolaythebest@users.noreply.github.com>
2025-03-16 22:44:17 +0100

    filter correct 4

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -13,7 +13,7 @@ export const getAllContacts = async ({
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactsQuery = ContactsCollection.find();
+  const contactsQuery = ContactsCollection.find(filter);
 
   if(filter.type) {
     contactsQuery.where('type').equals(filter.type);
