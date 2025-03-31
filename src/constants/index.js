2b69e05a0759b63f284bb25ac55d8364c178a05b
Nickolaythebest <156540277+Nickolaythebest@users.noreply.github.com>
2025-04-01 00:37:58 +0200

    cloudinary 2

--- src/constants/index.js
+++ src/constants/index.js
@@ -27,7 +27,7 @@ export const SORT_ORDER = {
   export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
   export const CLOUDINARY = {
-    CLOUD_NAME: 'CLOUDINARY_CLOUD_NAME',
-    API_KEY: 'CLOUDINARY_API_KEY',
-    API_SECRET: 'CLOUDINARY_API_SECRET'
+    CLOUD_NAME: 'CLOUD_NAME',
+    API_KEY: 'API_KEY',
+    API_SECRET: 'API_SECRET'
   };
